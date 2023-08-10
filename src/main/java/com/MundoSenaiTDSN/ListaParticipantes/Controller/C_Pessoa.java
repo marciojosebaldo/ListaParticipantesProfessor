5eaab192e95cb861140dbdf6bfa50c41fa6f82d4
Lucas Fernandes Braz <lucas.braz@sistemafiep.org.br>
2023-08-10 13:53:04 -0300

    Adição de mensagens de erro no cadastro

--- src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Controller/C_Pessoa.java
+++ src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Controller/C_Pessoa.java
@@ -1,7 +1,9 @@
 package com.MundoSenaiTDSN.ListaParticipantes.Controller;
 
+import com.MundoSenaiTDSN.ListaParticipantes.Model.M_Resposta;
 import com.MundoSenaiTDSN.ListaParticipantes.Service.S_Pessoa;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,9 +33,15 @@ public class C_Pessoa {
                                @RequestParam("telefone") String telefone,
                                @RequestParam("email") String email,
                                @RequestParam("senha") String senha,
-                               @RequestParam("confSenha") String confSenha){
-        S_Pessoa.cadastrarPessoa(nome,cpf,email,telefone,senha, confSenha);
-        return "redirect:/";
+                               @RequestParam("confSenha") String confSenha, Model model){
+        M_Resposta m_resposta = S_Pessoa.cadastrarPessoa(nome,cpf,email,telefone,senha, confSenha);
+        if(m_resposta.getStatus()) {
+            model.addAttribute("mensagem",m_resposta.getMensagem());
+            return "Login/login";
+        }else{
+            model.addAttribute("mensagem",m_resposta.getMensagem());
+            return "Cad_pessoa/cad_pessoa";
+        }
     }
 
 }
--- src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Service/S_Pessoa.java
+++ src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Service/S_Pessoa.java
@@ -1,7 +1,9 @@
 package com.MundoSenaiTDSN.ListaParticipantes.Service;
 
 import com.MundoSenaiTDSN.ListaParticipantes.Model.M_Pessoa;
+import com.MundoSenaiTDSN.ListaParticipantes.Model.M_Resposta;
 import com.MundoSenaiTDSN.ListaParticipantes.Repository.R_Pessoa;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,24 +14,24 @@ public class S_Pessoa {
         this.r_pessoa = r_pessoa;
     }
 
-    public static String cadastrarPessoa(String nome, String cpf, String email, String telefone, String senha, String conf_senha){
+    public static M_Resposta cadastrarPessoa(String nome, String cpf, String email, String telefone, String senha, String conf_senha){
         String mensagem = "";
         boolean podeSalvar = true;
 
         if(!senha.equals(conf_senha) || senha == null || senha.trim().equals("")){
-            mensagem += "Senha e confirmação de devem ser iguais, e a senha deve ser informada";
+            mensagem += "Senha e confirmação de devem ser iguais, e a senha deve ser informada<br/>";
             podeSalvar = false;
         }
         if(!S_ValidadorCPF.validarCPF(cpf)){
-            mensagem += "CPF inválido";
+            mensagem += "CPF inválido<br/>";
             podeSalvar = false;
         }
         if(nome == null || nome.trim().equals("")){
-            mensagem += "O nome precisa ser informado";
+            mensagem += "O nome precisa ser informado<br/>";
             podeSalvar = false;
         }
         if((email == null || email.trim().equals("")) && (telefone == null || telefone.trim().equals(""))){
-            mensagem += "e-Mail ou Telefone precisa ser informado";
+            mensagem += "e-Mail ou Telefone precisa ser informado<br/>";
             podeSalvar = false;
         }
 
@@ -39,10 +41,21 @@ public class S_Pessoa {
             m_pessoa.setEmail(email);
             m_pessoa.setSenha(senha);
             m_pessoa.setCpf(Long.valueOf(S_LimpaNumero.limpar(cpf)));
-            m_pessoa.setTelefone(Long.valueOf(S_LimpaNumero.limpar(telefone)));
-            r_pessoa.save(m_pessoa);
-            mensagem += "Pessoa Cadastrada Com Sucesso";
+            telefone = S_LimpaNumero.limpar(telefone);
+            if(telefone == ""){
+                m_pessoa.setTelefone(null);
+            }else{
+                m_pessoa.setTelefone(Long.valueOf(telefone));
+            }
+            try {
+                r_pessoa.save(m_pessoa);
+                mensagem += "Pessoa Cadastrada Com Sucesso";
+            }catch (DataIntegrityViolationException e){
+                mensagem += e.getMessage()+"<br/>";
+                podeSalvar = false;
+            }
         }
-        return mensagem;
+        M_Resposta m_resposta = new M_Resposta(podeSalvar,mensagem);
+        return m_resposta;
     }
 }
