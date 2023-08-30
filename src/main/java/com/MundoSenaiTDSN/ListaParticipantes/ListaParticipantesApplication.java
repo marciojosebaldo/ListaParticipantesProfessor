422f80b5af0d1fdc5ff2976da5e88aa5ff816f22
Lucas Fernandes Braz <lucas.braz@sistemafiep.org.br>
2023-08-30 19:23:37 -0300

    atividade correção requisição

--- src/main/java/com/MundoSenaiTDSN/ListaParticipantes/ListaParticipantesApplication.java
+++ src/main/java/com/MundoSenaiTDSN/ListaParticipantes/ListaParticipantesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ListaParticipantesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ListaParticipantesApplication.class, args);
 	}
