019643f2904aabfdea2e84c9fb40d0efc35349cc
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-30 20:12:06 -0300

    C_Pessoa

---
+++ src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Model/M_Form_Cadastro.java
@@ -0,0 +1,11 @@
+package com.MundoSenaiTDSN.ListaParticipantes.Model;
+
+public class M_Form_Cadastro {
+    private long id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String confSenha;
+}
