22f03152f6d1f6b63a139561fa7240af7bd01490
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-06 21:39:54 -0300

    HTML e JS

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,5 +2,7 @@ $("#btnCadastrar").click(function(event){
     event.preventDefault();
     $.get("/cadastro", function(data) {
         $(".container").html(data);
+        $("#btnEnviar").click(validaEnvio);
+        history.pushState({}, '', "/cadastro"); // Pode manipular a URL em evento padrão
     });
 });
